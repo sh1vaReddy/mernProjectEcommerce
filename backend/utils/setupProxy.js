59e14b44e285ac18ca33f6ac15dee71e5fb3726b
Kanagadda shiva prasad reddy <139767341+sh1vaReddy@users.noreply.github.com>
2024-03-30 14:28:08 +0530

    Update

--- backend/utils/setupProxy.js
+++ backend/utils/setupProxy.js
@@ -1,7 +1,1 @@
-const proxy = require("http-proxy-middleware");
-module.exports = function(app) {
-  app.use(proxy("/api/**", { // https://github.com/chimurai/http-proxy-middleware
-    target: "http://127.0.0.1:4000",
-    secure: false
-  }));
-};
+
